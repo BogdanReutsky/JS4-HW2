18e7bc1cc79a3e49e55a308a771a04eb7c92cf2d
BogdanReutsky <reutbodya@gmail.com>
2026-04-01 18:04:49 +0300

    push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const sliderEl = document.querySelector(".slider__input")
 const imgEl = document.querySelector(".slider__image")
 console.log(sliderEl, imgEl);
 
-sliderEl.addEventListener("input", _.debounce((event)=>{
+sliderEl.addEventListener("input", _.debounce ((event)=>{
 console.log(event.target.value);
 imgEl.style.width = event.target.value+"px";
 
